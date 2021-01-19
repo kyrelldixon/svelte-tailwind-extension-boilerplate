51748aa7ff364a91174ff61b8037eccedfadca96
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-19 11:48:27 -0600

    feat: move counter logic to component and style

---
+++ src/popup/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+  let count = 0;
+  const increment = () => (count += 1);
+  const decrement = () => (count -= 1);
+</script>
+
+<main>
+  <p class="text-xl text-center">{count}</p>
+  <div class="flex text-xl text-white">
+    <button class="px-4 py-1 bg-red-500" on:click={decrement}>-</button>
+    <button class="px-4 py-1 bg-green-500" on:click={increment}>+</button>
+  </div>
+</main>
--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -1,13 +1,7 @@
 <script>
-  let count = 0;
-  const increment = () => (count += 1);
-  const decrement = () => (count -= 1);
+  import Counter from "./Counter.svelte";
 </script>
 
-<main>
-  <div>
-    <button on:click={decrement}>-</button>
-    <button on:click={increment}>+</button>
-  </div>
-  <p>{count}</p>
+<main class="p-2">
+  <Counter />
 </main>
