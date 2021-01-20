c7593bfa6b049938dd48751505c585a5119ee93c
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-20 07:27:49 -0600

    chore(tests): Configure jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    "^.+\\.svelte$": "svelte-jester",
+    "^.+\\.js$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "svelte"],
+  setupFilesAfterEnv: ["<rootDir>/scripts/setupTests.js"],
+};
